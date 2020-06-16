e2d9b31d38745b4a6fbc7433b8f2a3bd8253986c
Hadesknight <fernandorato.0@hotmail.com>
2020-06-16 14:31:39 -0300

    Create style to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImage from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +17,56 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/46356996?s=460&u=056040d09322e9876e2563392621435f2c6a367f&v=4"
+            alt="githubProfile"
+          />
+          <div>
+            <strong>Fernando Santos</strong>
+            <p>Conteudo do Repositorio contendo tudo que vem escrito</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/46356996?s=460&u=056040d09322e9876e2563392621435f2c6a367f&v=4"
+            alt="githubProfile"
+          />
+          <div>
+            <strong>Fernando Santos</strong>
+            <p>Conteudo do Repositorio contendo tudo que vem escrito</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/46356996?s=460&u=056040d09322e9876e2563392621435f2c6a367f&v=4"
+            alt="githubProfile"
+          />
+          <div>
+            <strong>Fernando Santos</strong>
+            <p>Conteudo do Repositorio contendo tudo que vem escrito</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/46356996?s=460&u=056040d09322e9876e2563392621435f2c6a367f&v=4"
+            alt="githubProfile"
+          />
+          <div>
+            <strong>Fernando Santos</strong>
+            <p>Conteudo do Repositorio contendo tudo que vem escrito</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
